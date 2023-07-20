50a8f3d94763af8e5a49716aed39b99688980f09
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-20 18:59:22 +0300

    Debug

--- Demo3/Program.cs
+++ Demo3/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Enter a number");
+            //string input = Console.ReadLine();
+            //int number = Convert.ToInt32(input);
+
+            //if (number == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Your number was not zero");
+            //}
+            //Console.WriteLine("Done");
+
+            // 2/2
+            // % -reminder
+            //Console.WriteLine(4%2);
+            //Console.WriteLine(5%2);
+            Console.WriteLine("Enter the number");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            int reminder = number % 2;//getting the reminder
+            if (reminder == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
         }
     }
 }
---
+++ Demo4/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Demo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            //If statement
+            // if ( expression) // condition
+            //{
+            // statement block
+            //}
+            if (number == 2)
+            {​​​​​
+                Console.WriteLine("Yes");
+            }​​​​​
+            else
+            {​​​​​
+                Console.WriteLine("No");
+            }​​​​​
+        }
+    }
+}
