ef1c55bda9d85bfc209fc0afb1461d8550dbd1ad
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-21 10:55:10 +0300

    New Project

---
+++ Demo5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number?");
+            string? input = Console.ReadLine();
+            int number  = Convert.ToInt32(input);
+
+            if(number== 4)
+            {
+                Console.WriteLine($"Your number is {input} and is equal to four.");
+            }
+            else
+            {
+                Console.WriteLine($"Your number is {input}");
+            }
+        }
+    }
+}
