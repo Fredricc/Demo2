142600d5ea50573fdecc056f6d5c2586fff4cda6
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-22 22:01:01 +0300

    new

--- Demo5/Program.cs
+++ Demo5/Program.cs
@@ -5,18 +5,17 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter a number?");
-            string? input = Console.ReadLine();
-            int number  = Convert.ToInt32(input);
+            Console.WriteLine("Enter your full name");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter your home address:");
+            string input2 = Console.ReadLine();
+            Console.WriteLine("Enter your Id");
+            string input3 = Console.ReadLine();
 
-            if(number== 4)
-            {
-                Console.WriteLine($"Your number is {input} and is equal to four.");
-            }
-            else
-            {
-                Console.WriteLine($"Your number is {input}");
-            }
+            int idNumber = Convert.ToInt32(input3);
+
+            Console.WriteLine($"Mr {input1} your home address is {input2}");
+            Console.WriteLine($"Salary is {idNumber * 40}");
         }
     }
 }
