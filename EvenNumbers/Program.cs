1e968cd88bcb0be4413280402523facd58a69660
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-24 13:24:30 +0300

    exclude0and100

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            string? input1 = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(input1);
+
+            int reminder = firstNumber % 2;
+
+            if (reminder == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+        }
+    }
+}
---
+++ exclude0and100/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exclude0and100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            string? input1 = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(input1);
+
+            if (firstNumber >= 0 && firstNumber <= 100)
+            {
+                Console.WriteLine("Number is within 0 to 100.");
+            }
+            else
+            {
+                Console.WriteLine("Number is not within 0 to 100.");
+            }
+        }
+    }
+}
