08e0cf8b4920aebf5893502e9d0699651e168f49
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-24 09:31:18 +0300

    Contributed to Fredricc

--- Demo5/Program.cs
+++ Demo5/Program.cs
@@ -4,18 +4,16 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter you names");
+            string? input1 = Console.ReadLine();
 
-            Console.WriteLine("Enter your full name");
-            string input1 = Console.ReadLine();
-            Console.WriteLine("Enter your home address:");
-            string input2 = Console.ReadLine();
-            Console.WriteLine("Enter your Id");
-            string input3 = Console.ReadLine();
+            Console.WriteLine( "Enter your ID:");
+            string? input2 = Console.ReadLine();
 
-            int idNumber = Convert.ToInt32(input3);
+            int id = Convert.ToInt32(input2);
+            Console.WriteLine($"Hello {input1} your ID {id} has been added");
 
-            Console.WriteLine($"Mr {input1} your home address is {input2}");
-            Console.WriteLine($"Salary is {idNumber * 40}");
+
         }
     }
 }
