eb671702041868a88d590fd419aaea658717d4a5
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-26 19:57:22 +0300

    while loop for even numbers

---
+++ While_Loop/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+
+            //while (i <=10)// always
+            //{
+            //    Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            int i = 0;
+            while (true)
+            {
+                if ( i % 2 == 0)
+            {
+                Console.WriteLine(i);
+
+            }
+                if ( i == 50)
+                {
+                    break;
+                }
+               ++ i;
+            }
+
+        }
+    }
+}
